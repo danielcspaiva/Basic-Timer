52a3ff15c460a28e8cc3cd91778c298c0b24e8b5
Daniel Paiva <danielcspaiva@gmail.com>
2021-07-18 03:55:07 -0300

    fix 100vh for mobile browsers

--- src/styles.js
+++ src/styles.js
@@ -2,8 +2,16 @@ import styled from "styled-components";
 
 const backgroundColor = '#fdfffc'
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+window.addEventListener("resize", () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
 export const Container = styled.div`
     height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
     display: flex;
     flex-direction: column;
     justify-content: space-between;;
